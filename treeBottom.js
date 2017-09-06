4fee1d176acb19a206e132cda6ec56039dc3882d
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-09-06 09:26:55 -0700

    tree bottom working i think

--- treeBottom.js
+++ treeBottom.js
@@ -1,53 +1,97 @@
 var Node = function(val) {
-  this.value = val;
+  this.value = val || null;
   this.left = null;
   this.right = null;
 }
 
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+
+  push(val) {
+    this.storage.push(val);
+  }
+
+  pop() {
+    return this.storage.pop();
+  }
+
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+
+  peek() {
+    return this.storage[this.storage.length - 1];
+  }
+}
+
+
 function treeBottom(str) {
 
-  var tree = new Node(str[1]);
-  str = str.slice(3);
-  // console.log(str);
 
-  var leftNodeClosed = false;
-  var buildTreeFromString = (node, str) => {
-    // console.log('currentNode', node);
-    if (str[0] === '(') {
-      if (str[1] !== ')') {
-        if (!leftNodeClosed) {
-          node.left = new Node(str[1]);
-          buildTreeFromString(node.left, str.slice(3));
-        } else {
-          node.right = new Node(str[1]);
-          buildTreeFromString(node.right, str.slice(3));
-        }
-      } else {
-        leftNodeClosed = !leftNodeClosed;
+  var stack = new Stack();
+
+  var buildTreeFromString = (str) => {
+    var i = 0;
+    while (i < str.length - 1) {
+      // trim whitespace
+      if (str[i] === ' ') {
+        i++;
       }
-    }
-    if (str[0] === ' ') {
-      console.log('no')
-    }
-    if (str[0] === ')') {
-     console.log('no')
-    }
-  };
 
-  buildTreeFromString(tree, str);
+      if (str[i] === '(' && str[i + 1] === ')') {
+        i += 2;
+      }
+      if (str[i] === ')') {
+        stack.pop();
+      }
 
-  console.log('tree', tree);
-}
+      if (str[i] === '(' && str[i + 1] !== ')') {
+        var currentNode = new Node(str[i + 1]);
+        if (!stack.isEmpty()) {
+          var parentNode = stack.peek();
+          if (parentNode.left !== null) {
+            parentNode.right = currentNode;
+          } else {
+            parentNode.left = currentNode;
+          }
+        }
+        stack.push(currentNode);
+      }
+      i++;
+    }
+  }
+
+  buildTreeFromString(str);
+  // console.log('tree', tree);
+  return stack.peek();
+
+};
+
+
+//
+//            2
+//          /   \
+//        /       \
+//      7          5
+//    /   \         \
+//   /     \         \
+//  2       6        9
+//        /   \      /
+//       /     \    /
+//       5     11  4
+
 
 var tree1 = "(2 (7 (2 () ()) (6 (5 () ()) (11 () ()))) (5 () (9 (4 () ()) ())))";
 var tree1ExpectedOutput = [5, 11, 4];
-// var tree1Actual = treeBottom(tree1);
-// console.log('tree1', tree1Actual);
+var tree1Actual = treeBottom(tree1);
+console.log('tree1', tree1Actual);
 
-var tree2 = '(2 (1 () ()) (3 () ())'
+var tree2 = '(2 (1 () ()) (3 () ())';
 var tree2ExpectedOutput = [2, 3];
 var tree2Actual = treeBottom(tree2);
-console.log('tree2', tree2Actual);
+// console.log('tree2', tree2Actual);
 
 var tree3 = '(1 () ())'
 var tree3ExpectedOutput = [1];
