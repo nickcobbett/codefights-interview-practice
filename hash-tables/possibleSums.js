e64acfae244f99ca915e8f918b3de66c13fb37fd
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-09-16 16:59:57 -0700

    passing the 5th test but not passing other tests

--- hash-tables/possibleSums.js
+++ hash-tables/possibleSums.js
@@ -4,49 +4,46 @@ function possibleSums(coins, quantity) {
   var count = 0;
 
   coins.forEach((coin, i) => {
-    var coinStr = coin.toString();
-
-    if (!combos[coinStr]) {
-      combos[coinStr] = eval(coin);
-    }
-
-    if (!sums[coinStr]) {
-      count++;
-      sums[coinStr] = eval(coin);
-    }
-
+    var key1 = coin.toString();
+    combos[key1] = [];
     for (var q = 1; q <= quantity[i]; q++) {
-      for (var key in combos) {
-        var combo = `${key}+${coinStr}`; // big strings, make em smaller use different value for the sum
-
-        var sum = eval(combo);
-        if (key !== coinStr || q > 1) { // && !combos[combo] && !sums[sum]
-          // console.log(key, coinStr)
-          if (!combos[combo] && !sums[sum.toString()]) {
-            combos[combo] = sum;
-            sums[sum.toString()] = sum;
-            count++;
-          }
-        }
+      combos[key1].push(coin*q)
+      if (!sums[(coin*q).toString()]) {
+        sums[(coin*q).toString()] = 1;
+        count++;
       }
     }
-
+    combos[key1].forEach(sum1 => {
+      for (var key2 in combos) {
+        if (key2 === key1) {
+          continue;
+        }
+        combos[key2].forEach(sum2 => {
+          var sum3 = sum1 + sum2;
+          combos[key1].push(sum3);
+          if (!sums[sum3.toString()]) {
+            sums[sum3.toString()] = 1;
+            count++;
+          }
+        })
+      }
+    })
   });
 
-  // console.log('sums', sums);
   // console.log('combos', combos);
+  // console.log('sums', sums);
   return count;
 }
 
 var coins = [10, 50, 100];
 var quantity = [1, 2, 1];
-console.log('test1: ', possibleSums(coins, quantity)); // 9
+// console.log('test1: ', possibleSums(coins, quantity)); // 9
 coins = [10, 50, 100, 500];
 quantity = [5, 3, 2, 2];
-console.log('test2: ', possibleSums(coins, quantity)); // 122
+// console.log('test2: ', possibleSums(coins, quantity)); // 122
 coins = [1];
 quantity = [5];
-console.log('test3: ', possibleSums(coins, quantity)); // 5
+// console.log('test3: ', possibleSums(coins, quantity)); // 5
 coins = [1, 1];
 quantity = [2, 3];
 console.log('test4: ', possibleSums(coins, quantity)); // 5
