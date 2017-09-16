d13aee56a46a3b8d698972dba5e5149e1a02271f
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-09-16 11:09:40 -0700

    passing 4 of 5 tests

--- hash-tables/possibleSums.js
+++ hash-tables/possibleSums.js
@@ -17,7 +17,8 @@ function possibleSums(coins, quantity) {
 
     for (var q = 1; q <= quantity[i]; q++) {
       for (var key in combos) {
-        var combo = `${key}+${coinStr}`;
+        var combo = `${key}+${coinStr}`; // big strings, make em smaller use different value for the sum
+
         var sum = eval(combo);
         if (key !== coinStr || q > 1) { // && !combos[combo] && !sums[sum]
           // console.log(key, coinStr)
@@ -39,15 +40,16 @@ function possibleSums(coins, quantity) {
 
 var coins = [10, 50, 100];
 var quantity = [1, 2, 1];
-// console.log('test1: ', possibleSums(coins, quantity)); // 9
-
-
-
-coins = [10, 50, 100, 500]
-quantity = [5, 3, 2, 2]
+console.log('test1: ', possibleSums(coins, quantity)); // 9
+coins = [10, 50, 100, 500];
+quantity = [5, 3, 2, 2];
 console.log('test2: ', possibleSums(coins, quantity)); // 122
-
-
-coins = [1, 2]
-quantity = [50000, 2]
+coins = [1];
+quantity = [5];
+console.log('test3: ', possibleSums(coins, quantity)); // 5
+coins = [1, 1];
+quantity = [2, 3];
+console.log('test4: ', possibleSums(coins, quantity)); // 5
+coins = [1, 2];
+quantity = [50000, 2];
 // console.log('test5: ', possibleSums(coins, quantity)); // 50004
